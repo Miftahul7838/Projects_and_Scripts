a29cb276ee94d70db5861d65fb89960d27322e8e
Miftahul Huq <mh8872@rit.edu>
2022-01-25 19:23:52 -0500

    Added python pentesting projects

---
+++ python_pentesting_tools/Simple_Client_Server_connection/simpleUDPClient.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 9997
+
+# Create a UDP socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Send some data
+client.sendto(b"AAABBBCCC", (target_host, target_port))
+
+# Receive some data
+data, addr = client.recvfrom(4096)
+
+# Print the received data and close the client socket
+print(data.decode())
+client.close()
